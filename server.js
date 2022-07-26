a899c4ecdbc5a8838e1ab3245bf9f8c845648431
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-07-25 22:28:24 -0400

    fixed server port

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path');
 const api = require('./routes/index.js');
 const { v4: uuidv4 } = require('uuid');
 
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 var noteData = require('./db/db.json')
 
